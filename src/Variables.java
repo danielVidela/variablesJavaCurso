10f021871647dd53e0dcd12f5deb9640734b4fc7
Daniel Videla <daniel.videla.developer@gmail.com>
2022-08-13 12:23:28 -0300

    ejemplos de variables

--- src/Variables.java
+++ src/Variables.java
@@ -1,2 +1,15 @@
 public class Variables {
+    public static void main(String[] args) {
+        int variable1;
+        int variable2, variable3;
+        int variable4 = 23;
+
+        variable1 = 6;
+
+        System.out.println("variable1 contoene el valor "+variable1);
+
+        variable2 = 2 * variable1;
+
+        System.out.println("Variable dos contiene el valor: " + variable2);
+    }
 }
