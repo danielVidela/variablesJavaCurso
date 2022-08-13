bbf085001895bfc56830f14ef604cea5fcc5d8bb
Daniel Videla <daniel.videla.developer@gmail.com>
2022-08-13 13:41:03 -0300

    alcances de los bloques

--- src/AlcanceDeLasVariables.java
+++ src/AlcanceDeLasVariables.java
@@ -1,2 +1,11 @@
 public class AlcanceDeLasVariables {
+    public static void main(String[] args) {
+        int dias = 0;
+        while (dias<30){
+            dias = dias +1;
+            int semanas = dias / 7;
+            System.out.println("An pasado " + dias +" dias lo que supone "+ semanas+" semanas");
+
+        }
+    }
 }
