d822c0d8fda96b1a9bd167dfce9e117cbf0116c7
Daniel Videla <daniel.videla.developer@gmail.com>
2022-08-13 13:28:40 -0300

    literales y variables

---
+++ src/Literales.java
@@ -0,0 +1,21 @@
+public class Literales {
+    public static void main(String[] args) {
+        int enteroDecimal = 100;
+        int enteroOctal = 0100;
+        int enteroExadecimal = 0x100;
+        System.out.println("Entero exadecmal es "+ enteroExadecimal);
+        Long numeroLong = 100L;
+        double numeroDecimal = 2.5;
+        float numeroFloat = 2.5f;
+        int numeroConGuiones = 25_258_236;
+        System.out.println("El numero con guiones es: "+ numeroConGuiones);
+        char miChar = 'b';
+        char miOtroChar ='\u0062';
+        System.out.println("el caracter mim optro char es: "+miOtroChar);
+        char comiillaSimple = '\'';
+        String stringLiteral = "Esto es un Literal";
+        boolean booleanVerdadero = true;
+        String stringNulo = null;
+
+    }
+}
